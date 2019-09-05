413f9fcf1613e7d7a8004190212202bd104becbb
ClarisseU <clarisseum@gmail.com>
2019-09-05 15:41:39 +0200

    create 2 classes

---
+++ user.py
@@ -0,0 +1,10 @@
+class User:
+    '''
+    Tis class is going to hold and generate all the informations about the user
+    '''
+    
+    
+class Credentials:
+    '''
+    This class is going to hold information about the credentials
+    '''    
