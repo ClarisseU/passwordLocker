aecce92ca782b1050d33ecf7a42a7978fd7dfc08
ClarisseU <clarisseum@gmail.com>
2019-09-05 15:51:18 +0200

    add init method

--- user.py
+++ user.py
@@ -2,9 +2,17 @@ class User:
     '''
     Tis class is going to hold and generate all the informations about the user
     '''
+    def __init__(self,username,password,email):
+        '''
+        this method helps to define the properties for our objects.
+        '''
     
     
 class Credentials:
     '''
     This class is going to hold information about the credentials
-    '''    
+    '''
+    def __init__(self,username,password,email):
+        '''
+        will hold the properties for our objects
+        '''    
