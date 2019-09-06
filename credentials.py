cb8a64f1b0f38cd7e187023e4a39068feb3dc1ba
ClarisseU <clarisseum@gmail.com>
2019-09-06 12:00:58 +0200

    add __init__ method

---
+++ credentials.py
@@ -0,0 +1,13 @@
+class Credentials:
+    '''
+    This class is going to hold information about the credentials
+    '''
+    cred_list = []
+    def __init__(self,socialMed,username,password,email):
+        '''
+        will hold the properties for our objects
+        '''    
+        self.socialMed = socialMed
+        self.username = username
+        self.password = password
+        self.email = email
