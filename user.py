c9d10118045597c23495bcb25376cd635302eb2a
ClarisseU <clarisseum@gmail.com>
2019-09-06 10:15:30 +0200

    add the empty array list

--- user.py
+++ user.py
@@ -2,17 +2,26 @@ class User:
     '''
     Tis class is going to hold and generate all the informations about the user
     '''
+    user_list = []
     def __init__(self,username,password,email):
         '''
         this method helps to define the properties for our objects.
         '''
+        self.username = username
+        self.password = password
+        self.email = email
     
     
 class Credentials:
     '''
     This class is going to hold information about the credentials
     '''
-    def __init__(self,username,password,email):
+    cred_list = []
+    def __init__(self,socialMed,username,password,email):
         '''
         will hold the properties for our objects
         '''    
+        self.socialMed = socialMed
+        self.username = username
+        self.password = password
+        self.email = email
